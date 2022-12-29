6a014ea8007a831fc78e0e57b2be763045d7c57e
Ibrahim ben Salah <Ibrahim.BenSalah@planonsoftware.com>
2022-12-29 20:25:02 +0100

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,27 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(__dirname, './lib'),
+      },
+    ],
+  },
   build: {
-    manifest: true,
     minify: true,
     reportCompressedSize: true,
     lib: {
-      entry: path.resolve(__dirname, 'index.ts'),
+      entry: path.resolve(__dirname, 'lib/index.ts'),
       fileName: 'main',
-      formats: ['es', 'cjs'],
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        sourcemap: true,
+      },
     },
   },
 });

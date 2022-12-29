0130847f6e98eb4bc4011fbb0aec1e9dc775f98f
Ibrahim ben Salah <Ibrahim.BenSalah@planonsoftware.com>
2022-12-29 02:32:23 +0100

    build settings

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+﻿// vite.config.ts
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      fileName: 'main',
+      formats: ['es', 'cjs'],
+    },
+  },
+});
